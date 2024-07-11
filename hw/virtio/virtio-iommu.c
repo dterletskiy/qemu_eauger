qemu/qemu

commit 1b889d6e39c32d709f1114699a014b381bcf1cb1

virtio-iommu: Clear IOMMUDevice when VFIO device is unplugged
When a VFIO device is hoplugged in a VM using virtio-iommu, IOMMUPciBus
and IOMMUDevice cache entries are created in the .get_address_space()
handler of the machine IOMMU device. However, these entries are never
destroyed, not even when the VFIO device is detached from the machine.
This can lead to an assert if the device is reattached again.

When reattached, the .get_address_space() handler reuses an
IOMMUDevice entry allocated when the VFIO device was first attached.
virtio_iommu_set_host_iova_ranges() is called later on from the
.set_iommu_device() handler an fails with an assert on 'probe_done'
because the device appears to have been already probed when this is
not the case.

The IOMMUDevice entry is allocated in pci_device_iommu_address_space()
called from under vfio_realize(), the VFIO PCI realize handler. Since
pci_device_unset_iommu_device() is called from vfio_exitfn(), a sub
function of the PCIDevice unrealize() handler, it seems that the
.unset_iommu_device() handler is the best place to release resources
allocated at realize time. Clear the IOMMUDevice cache entry there to
fix hotplug.

Fixes: 817ef10da23c ("virtio-iommu: Implement set|unset]_iommu_device() callbacks")
Signed-off-by: Cédric Le Goater <clg@redhat.com>
Message-Id: <20240701101453.203985-1-clg@redhat.com>
Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

hw/virtio/virtio-iommu.c
@@ -467,6 +467,26 @@ static AddressSpace *virtio_iommu_find_add_as(PCIBus *bus, void *opaque,
     return &sdev->as;
 }
 
+static void virtio_iommu_device_clear(VirtIOIOMMU *s, PCIBus *bus, int devfn)
+{
+    IOMMUPciBus *sbus = g_hash_table_lookup(s->as_by_busptr, bus);
+    IOMMUDevice *sdev;
+
+    if (!sbus) {
+        return;
+    }
+
+    sdev = sbus->pbdev[devfn];
+    if (!sdev) {
+        return;
+    }
+
+    g_list_free_full(sdev->resv_regions, g_free);
+    sdev->resv_regions = NULL;
+    g_free(sdev);
+    sbus->pbdev[devfn] = NULL;
+}
+
 static gboolean hiod_equal(gconstpointer v1, gconstpointer v2)
 {
     const struct hiod_key *key1 = v1;
@@ -650,6 +670,7 @@ virtio_iommu_unset_iommu_device(PCIBus *bus, void *opaque, int devfn)
     }
 
     g_hash_table_remove(viommu->host_iommu_devices, &key);
+    virtio_iommu_device_clear(viommu, bus, devfn);
 }
 
 static const PCIIOMMUOps virtio_iommu_ops = {

dterletskiy/qemu_eauger

commit 36f7a5e42bb95eca509b0df6d7e30ee7705a4b0d

virtio-iommu: Fix IOMMUDevice cleanup
All the memory regions and the address space associated with
an IOMMUDevice also need to be cleaned up on virtio_iommu_device_clear()

Fixes: 1b889d6e39c3 ("virtio-iommu: Clear IOMMUDevice when VFIO device
is unplugged")
Signed-off-by: Eric Auger <eric.auger@redhat.com>

hw/virtio/trace-events
@@ -137,6 +137,7 @@ virtio_iommu_notify_flag_del(const char *name) "del notifier from mr %s"
 virtio_iommu_switch_address_space(uint8_t bus, uint8_t slot, uint8_t fn, bool on) "Device %02x:%02x.%x switching address space (iommu enabled=%d)"
 virtio_iommu_freeze_granule(uint64_t page_size_mask) "granule set to 0x%"PRIx64
 virtio_iommu_host_resv_regions(const char *name, uint32_t index, uint64_t lob, uint64_t upb) "mr=%s host-resv-reg[%d] = [0x%"PRIx64",0x%"PRIx64"]"
+virtio_iommu_device_clear(const char *name) "mr=%s"
 
 # virtio-mem.c
 virtio_mem_send_response(uint16_t type) "type=%" PRIu16

hw/virtio/virtio-iommu.c
@@ -480,11 +480,22 @@ static void virtio_iommu_device_clear(VirtIOIOMMU *s, PCIBus *bus, int devfn)
     if (!sdev) {
         return;
     }
+    sdev->probe_done = false;
+    g_list_free_full(g_steal_pointer(&sdev->resv_regions), g_free);
+    g_list_free_full(g_steal_pointer(&sdev->host_resv_ranges), g_free);
 
-    g_list_free_full(sdev->resv_regions, g_free);
-    sdev->resv_regions = NULL;
-    g_free(sdev);
+    trace_virtio_iommu_device_clear(sdev->iommu_mr.parent_obj.name);
+    memory_region_del_subregion(MEMORY_REGION(&sdev->root),
+                                MEMORY_REGION(&sdev->bypass_mr));
+    memory_region_del_subregion(MEMORY_REGION(&sdev->root),
+                                MEMORY_REGION(&sdev->iommu_mr));
+    memory_region_unref(MEMORY_REGION(&sdev->iommu_mr));
+    memory_region_unref(MEMORY_REGION(&sdev->bypass_mr));
+    address_space_remove_listeners(&sdev->as);
+    /* destroy the address space. This also destroys its root memory region */
+    address_space_destroy(&sdev->as);
     sbus->pbdev[devfn] = NULL;
+    g_free(sdev);
 }
 
 static gboolean hiod_equal(gconstpointer v1, gconstpointer v2)